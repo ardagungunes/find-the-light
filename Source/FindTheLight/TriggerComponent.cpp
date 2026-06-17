46a18ed8a233fb60d557e34d5b55877d5651c935
Arda Gün Güneş <ardagungunes@gmail.com>
2026-06-17 23:41:57 +0200

    Added a connection to the delegate OnComponentOverlapBegin, OnComponentOverlapEnd

--- Source/FindTheLight/TriggerComponent.cpp
+++ Source/FindTheLight/TriggerComponent.cpp
@@ -12,11 +12,48 @@ void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
+	if (moverActor) {
+		
+		mover = moverActor->FindComponentByClass<UMover>();
+
+		if (mover) {
+			UE_LOG(LogTemp, Display, TEXT("Successfully found the mover component."));
+		}
+		else {
+			UE_LOG(LogTemp, Warning, TEXT("Failed to find mover component."));
+		}
+
+	}
+
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Mover Actor is nullptr."));
+	}
+
+	if (isPressurePlate) {
+		OnComponentBeginOverlap.AddDynamic(this, &UTriggerComponent::OnOverlapBegin);
+		OnComponentEndOverlap.AddDynamic(this, &UTriggerComponent::OnOverlapEnd);
+	}
 	
 }
 
 void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+}
+
+void UTriggerComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (mover) {
+		mover->shouldMove = true;
+	}
+	
+}
+
+void UTriggerComponent::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (mover) {
+		mover->shouldMove = false;
+	}
 	
 }
--- Source/FindTheLight/TriggerComponent.h
+++ Source/FindTheLight/TriggerComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/BoxComponent.h"
+#include "Mover.h"
 #include "TriggerComponent.generated.h"
 
 /**
@@ -25,4 +26,18 @@ protected:
 public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere) AActor* moverActor;
+
+	UMover* mover;
+
+	/** called when something enters the sphere component */
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	/** called when something leaves the sphere component */
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(EditAnywhere) bool isPressurePlate = false;
 };
