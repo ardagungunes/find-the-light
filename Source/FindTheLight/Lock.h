ed718cae73bd9cd57080f3b33cc7ac780642a575
Arda Gün Günes <ardagungunes@gmail.com>
2026-07-06 23:18:38 +0200

    Created the Lock Actor from c++ and it's Blueprint

---
+++ Source/FindTheLight/Lock.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "TriggerComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+#include "Lock.generated.h"
+
+UCLASS()
+class FINDTHELIGHT_API ALock : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALock();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* rootComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UTriggerComponent* triggerComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* keyItemMesh;
+
+};
